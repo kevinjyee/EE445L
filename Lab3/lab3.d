833cb568ed623de0cc8dcbea932cb507749a736a
sbordov <stefan.bordovsky@gmail.com>
2017-02-06 18:19:55 -0600

    Renamed project and took compiled files out of repo for cleaner commits!

--- Lab3/lab3.d
+++
@@ -1,7 +0,0 @@
-.\lab3.o: Lab3.c
-.\lab3.o: E:\Keil1\ARM\ARMCC\bin\..\include\stdint.h
-.\lab3.o: ADCSWTrigger.h
-.\lab3.o: ../inc/tm4c123gh6pm.h
-.\lab3.o: PLL.h
-.\lab3.o: ST7735.h
-.\lab3.o: fixed.h
