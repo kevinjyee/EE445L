79134fb8dc730413c8a373523ced2d8468483b8a
sbordov <stefan.bordovsky@gmail.com>
2017-02-21 19:10:48 -0600

    Started defining the song struct.

--- Lab5/Music.c
+++ Lab5/Music.c
@@ -11,23 +11,46 @@
  
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
-#include "../SysTick.h"
+#include "SysTick.h"
 
 volatile uint8_t Note_Index = 0; 
 volatile uint8_t I = 0;
 volatile uint8_t J = 0;
 
+
+
+typedef struct
+{
+	int id;								// For use in menu selection
+	char name[20];					// For use in printing name to LCD.
+	int tempo;						// Sets tempo of song
+  int soprano_Notes[200];  // Soprano melody for song.
+	int alto_Notes[200];			// Alto melody for song.
+} Song;  /* Song structs  */
+
+// Holds a list of songs which may be indexed into using song id.
+typedef struct
+{
+	Song songs[];
+} Song_Choices;
+
 // ***************** toggle_Play ****************
 // Plays music if none playing, pauses music if already playing.
 // Inputs:  none
 // Outputs: none
+void toggle_Play(){
+}
 
 // ***************** change_Song ****************
 // Changes the song that is playing or begins playing the selected song.
 // Inputs:  none
 // Outputs: none
+void change_Song(){
+}
 
 // ***************** rewind ****************
 // Restart the current song.
 // Inputs:  none
 // Outputs: none
+void rewind(){
+}
